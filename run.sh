766f57887607d31dd2a77dc10291f24a562969fe
Leon Rinkel <leon@rinkel.me>
2024-03-08 09:53:11 +0000

    Update simulation runner

--- run.sh
+++ run.sh
@@ -1,38 +1,49 @@
 #!/bin/bash
 
+bits_options=(32 128 256)
+k_options=(10 20 40)
+
 function run_sim {
-    # simulate a couple of times
-    for i in `seq 1 10`
-    do
-        SEED=$RANDOM
+    for BITS in ${bits_options[@]}; do
+        for K in ${k_options[@]}; do
+            # simulate a couple of times
+            for i in `seq 1 10`
+            do
+                SEED=$RANDOM
 
-        # with/without sorting
-        for SORTING in `seq 0 1`
-        do
-            # create config
-            echo "" > example.cfg
-            echo "PDIST0 $PDIST0" >> example.cfg
-            echo "PDIST1 $PDIST1" >> example.cfg
-            echo "PDIST2 $PDIST2" >> example.cfg
-            echo "PDIST3 $PDIST3" >> example.cfg
-            echo "PDIST4 $PDIST4" >> example.cfg
-            echo "SEED $SEED" >> example.cfg
-            echo "SORTING $SORTING" >> example.cfg
-            cat baseline.cfg >> example.cfg
+                # with/without sorting
+                for SORTING in `seq 0 1`
+                do
+                    # create config
+                    echo "" > example.cfg
+                    echo "PDIST0 $PDIST0" >> example.cfg
+                    echo "PDIST1 $PDIST1" >> example.cfg
+                    echo "PDIST2 $PDIST2" >> example.cfg
+                    echo "PDIST3 $PDIST3" >> example.cfg
+                    echo "PDIST4 $PDIST4" >> example.cfg
+                    echo "SEED $SEED" >> example.cfg
+                    echo "SORTING $SORTING" >> example.cfg
+                    echo "BITS $BITS" >> example.cfg
+                    echo "K $K" >> example.cfg
+                    cat baseline.cfg >> example.cfg
 
-            # write log
-            file="run-$RANDOM.log"
-            echo "" > $file
-            echo "PDIST0 $PDIST0" >> $file
-            echo "PDIST1 $PDIST1" >> $file
-            echo "PDIST2 $PDIST2" >> $file
-            echo "PDIST3 $PDIST3" >> $file
-            echo "PDIST4 $PDIST4" >> $file
-            echo "SEED $SEED" >> $file
-            echo "SORTING $SORTING" >> $file
-            echo "" >> $file
+                    # write log
+                    file="run-$RANDOM.log"
+                    echo "" > $file
+                    echo "PDIST0 $PDIST0" >> $file
+                    echo "PDIST1 $PDIST1" >> $file
+                    echo "PDIST2 $PDIST2" >> $file
+                    echo "PDIST3 $PDIST3" >> $file
+                    echo "PDIST4 $PDIST4" >> $file
+                    echo "SEED $SEED" >> $file
+                    echo "SORTING $SORTING" >> $file
+                    echo "BITS $BITS" >> $file
+                    echo "K $K" >> $file
+                    echo "" >> $file
 
-            ./gradlew :app:run --args="example.cfg" >> $file 2>&1
+                    ./gradlew :app:run --args="example.cfg" >> $file 2>&1
+                done
+            done
         done
     done
 }
@@ -92,4 +103,3 @@ PDIST2=0.000
 PDIST3=0.700
 PDIST4=0.300
 run_sim
-
